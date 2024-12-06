ac533019587d9689e48e40ec24837c6eab9ffd45
Leedasol2 <dasol2267@naver.com>
2024-12-06 09:06:09 +0900

    섹션6 회원 관리 예제 - 웹 MVC 개발

--- hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hello_spring.controller;
 
+import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -21,18 +27,44 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    /*
     // 2. 필드 주입
-//    @Autowired
-//    private MemberService memberService;
+    @Autowired
+    private MemberService memberService;
 
 
     // 3. setter 주입
-//    private MemberService memberService;
-//    @Autowired
-//    public void setMemberService(MemberService memberService) {
-//        this.memberService = memberService;
-//    }
+    private MemberService memberService;
+    @Autowired
+    public void setMemberService(MemberService memberService) {
+        this.memberService = memberService;
+    }
+    */
 
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    // GetMapping 과 같은 주소지만 Post 함수를 통해 들어온 경로만 해당된다.
+    // 일반적으로 링크나 엔터로 동작할 때는 Get 함수이다
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
+    }
 
 
 
